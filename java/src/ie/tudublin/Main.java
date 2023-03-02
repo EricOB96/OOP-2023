1f5b82bd1e33a33ed70d51d8cec848903109059f
Eric <ericobrien246@gmail.com>
2023-03-02 12:21:43 +0000

    Audio2

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,5 +1,6 @@
 package ie.tudublin;
 
+
 public class Main
 {
 
@@ -32,6 +33,14 @@ public class Main
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Audio1());
     }
+
+	public static void Audio2()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio2());
+    }
+
+	
 	
 	public static void main(String[] args)
 	{
@@ -41,7 +50,9 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		audio1();
+		Audio2();
+
+		
 	}
 	
 }
